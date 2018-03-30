a4b9f0fb5bdeae00a1d7b0482125fd1970e5be42
Myshu <1291016966@qq.com>
2018-03-30 15:55:15 +0800

    Update app.R

--- R-shiny-apps/taxa_bar_plot/app.R
+++ R-shiny-apps/taxa_bar_plot/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Gene cluster"),
+  titlePanel("Bar Plot"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
